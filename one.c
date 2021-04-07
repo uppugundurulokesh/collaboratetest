3ee71cab2463c6f25b186ad9de9f3531fb2ab7a6
Krishna Murthy Naidu Bali <82026258+krishna0104@users.noreply.github.com>
2021-04-07 15:10:42 +0530

    Add files via upload

---
+++ one.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int a,b;
+	clrscr();
+	scanf("%d",&a);
+	scanf("%d",&b);
+	printf("sum = %d",a+b);
+	getch();
+	return 0;
+}	
+
---
+++ two.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+	int a=10,b=20;
+	int mul=a*b;
+	printf("mul = %d",mul);
+	return 0;
+
+}
